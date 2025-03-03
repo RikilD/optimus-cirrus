f1955eab16db8a04f6348aa4e5a53ff3defbdba5
Chris Andrews <chris.andrews@morganstanley.com>
2025-03-03 17:58:15 +0000

    Hardcoded support for protoc protobuf generator

--- optimus/buildtool/projects/app/src/main/scala/optimus/buildtool/generators/AnyBufGenerator.scala
+++ optimus/buildtool/projects/app/src/main/scala/optimus/buildtool/generators/AnyBufGenerator.scala
@@ -134,9 +134,13 @@ import scala.sys.process.ProcessLogger
       templates: SandboxedInputs,
       configuration: Map[String, String],
       scope: CompilationScope
-  ): AnyBufGenerator.Inputs = {
-    val executable = generatorDefaults.resolve(configuration, scope)
-    val execFingerprint = generatorDefaults.fingerprint(configuration, scope)
+  ): Inputs = {
+    val generator = generatorDefaults.configured(configuration)
+//    val execDep = generator.dependencyDefinition(scope)
+    val executable = generator.file()
+
+    // we don't want the platform-specific execDir to be part of the fingerprint
+    val execFingerprint = generatorDefaults.linux.file()
 
     val allTemplatePaths = templates.content().keySet.map(_.path)
 
