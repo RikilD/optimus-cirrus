9eeb7972be09cf978ab568af21cc4f037e2e23d7
Chris Andrews <chris.andrews@morganstanley.com>
2025-03-03 16:58:52 +0000

    Now able to build optimus.platform.core

--- optimus/platform/projects/core/src/main/scala/optimus/graph/Settings.java
+++ optimus/platform/projects/core/src/main/scala/optimus/graph/Settings.java
@@ -107,15 +107,8 @@ public class Settings {
   public static boolean keepTaskSchedulerAffinity =
       getBoolProperty("optimus.graph.keepTaskSchedulerAffinity", false);
 
-  /**
-   * if this flag is true, we will collect minimal PGO stats and APPLY the learned cache settings at
-   * runtime (see additional settings in OGAutoPGOObserver)
-   */
-  public static boolean livePGO = getBoolProperty("optimus.graph.livePGO", false);
-
-  public static boolean livePGO_GivenNodes = getBoolProperty("optimus.graph.livePGO.given", false);
-
-  public static final boolean useVirtualThreads = DiagnosticSettings.useVirtualThreads;
+  public static final boolean useVirtualThreads =
+      getBoolProperty("optimus.graph.useVirtualThreads", false);
   public static final boolean limitThreads =
       getBoolProperty("optimus.graph.limitThreads", !useVirtualThreads);
 
--- optimus/platform/projects/missing/src/main/scala/msjava/hdom/Element.scala
+++ optimus/platform/projects/missing/src/main/scala/msjava/hdom/Element.scala
@@ -6,7 +6,7 @@ class Element(name: String) {
   def getName: String = ???
   def getText: String = ???
   def getChildren(name: String): ju.List[Element] = ???
-  def getChildren(): ju.List[Element] = ???
+  def getChildren: ju.List[Element] = ???
   def getAttributes: ju.List[Attribute] = ???
   def getChildText(name: String): String = ???
   def getValueAsString: String = ???
